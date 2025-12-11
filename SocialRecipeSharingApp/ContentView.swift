3e267e8e6e8d5643e3a7cc8c273162957d75e13e
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-12-11 12:48:06 +0530

    initial commit

---
+++ SocialRecipeSharingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialRecipeSharingApp
+//
+//  Created by harsh chauhan on 11/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialRecipeSharingApp/SocialRecipeSharingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialRecipeSharingAppApp.swift
+//  SocialRecipeSharingApp
+//
+//  Created by harsh chauhan on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialRecipeSharingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
